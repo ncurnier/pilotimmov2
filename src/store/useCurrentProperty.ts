5bd8b9ae54f2a42b9d636a0f9dc6fdd08aae12c5
ncurnier <nicolascurnier@gmail.com>
2025-09-24 08:04:22 +0200

    Update useCurrentProperty.ts

--- src/store/useCurrentProperty.ts
+++ src/store/useCurrentProperty.ts
@@ -1,56 +1,18 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
-import logger from "../utils/logger";
-import type { Property } from "../services/supabase/types";
 
 type PropertyState = {
   currentPropertyId: string | null;
-  currentProperty: Property | null;
-  isPropertySelected: boolean;
-  setCurrentProperty: (property: Property) => void;
-  setCurrentPropertyId: (id: string | null) => void;
+  setCurrentProperty: (id: string) => void;
   clearCurrentProperty: () => void;
-  hasProperty: () => boolean;
-  getCurrentProperty: () => Property | null;
-  getCurrentPropertyId: () => string | null;
-};
-
-const initialState: Pick<
-  PropertyState,
-  "currentPropertyId" | "currentProperty" | "isPropertySelected"
-> = {
-  currentPropertyId: null,
-  currentProperty: null,
-  isPropertySelected: false,
 };
 
 export const useCurrentProperty = create<PropertyState>()(
   persist(
-    (set, get) => ({
-      ...initialState,
-      setCurrentProperty: (property) => {
-        logger.info("[Property] set", { id: property.id, address: property.address });
-        set({
-          currentPropertyId: property.id,
-          currentProperty: property,
-          isPropertySelected: true,
-        });
-      },
-      setCurrentPropertyId: (id) => {
-        logger.info("[Property] set id", id);
-        set({
-          currentPropertyId: id,
-          currentProperty: null,
-          isPropertySelected: Boolean(id),
-        });
-      },
-      clearCurrentProperty: () => {
-        logger.info("[Property] clear");
-        set({ ...initialState });
-      },
-      hasProperty: () => Boolean(get().currentPropertyId),
-      getCurrentProperty: () => get().currentProperty,
-      getCurrentPropertyId: () => get().currentPropertyId,
+    (set) => ({
+      currentPropertyId: null,
+      setCurrentProperty: (id) => set({ currentPropertyId: id }),
+      clearCurrentProperty: () => set({ currentPropertyId: null }),
     }),
     {
       name: "property-context",
@@ -61,40 +23,7 @@ export const useCurrentProperty = create<PropertyState>()(
           removeItem: () => {},
         }
       ),
-      partialize: (state) => ({
-        currentPropertyId: state.currentPropertyId,
-        currentProperty: state.currentProperty,
-        isPropertySelected: state.isPropertySelected,
-      }),
-      version: 2,
-      migrate: (persistedState, version) => {
-        if (!persistedState) {
-          return initialState;
-        }
-
-        const typedState = persistedState as Partial<PropertyState>;
-        const currentProperty = typedState.currentProperty ?? null;
-        const currentPropertyId =
-          typedState.currentPropertyId ?? currentProperty?.id ?? null;
-
-        if (version < 2) {
-          return {
-            ...initialState,
-            ...typedState,
-            currentProperty,
-            currentPropertyId,
-            isPropertySelected: Boolean(currentPropertyId),
-          };
-        }
-
-        return {
-          ...initialState,
-          ...typedState,
-          currentProperty,
-          currentPropertyId,
-          isPropertySelected: Boolean(currentPropertyId),
-        };
-      },
+      version: 1,
     }
   )
 );
